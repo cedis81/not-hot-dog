c7dae445da9a8debc71146f4324214beac04a65f
cedis81 <cedisweb0@gmail.com>
2021-08-26 17:53:51 -0400

    Create Count component
    * Create Count folder within components folder
    * Create Count component with temporary placeholder text
    * Import and render Count component in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Count from './components/Count/Count';
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
+        <Count />
         <ImageLinkForm />
       </div>
     );
---
+++ src/components/Count/Count.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Count = () => {
+  return (
+    <div>
+      <p>{`You've submitted X number of images, and Y number of them were hot dogs`}</p>
+    </div>
+  );
+}
+
+export default Count;
