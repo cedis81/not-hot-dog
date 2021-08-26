6fa9c5dfef06f24936a3bf0b5cf34d50a2a2e86e
cedis81 <cedisweb0@gmail.com>
2021-08-26 13:00:33 -0400

    Create Navigation component
    * Create components folder
    * Create Navigation folder within components
    * Create Navigation component with sign out text
    * Create Navigation css file with basic styling
    * Import and render Navigation component in App.js
    * Deleted logo.svg per prior commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>hi</h1>
+        <Navigation />
       </div>
     );
-  };
+  }
 }
 
 export default App;
