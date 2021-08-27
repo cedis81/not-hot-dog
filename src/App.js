122d384bfacf9f9db0abe50dc928ecda8fac51e9
cedis81 <cedisweb0@gmail.com>
2021-08-27 12:43:58 -0400

    Create onInputChange and onButtonSubmit methods
    * Create onInputChange in App.js with temporary console log
    * Add onInputChange as prop to ImageLinkForm
    * Create onButtonSubmit in App.js with temporary console log
    * Add onButtonSubmit as prop to ImageLinkForm
    * Destructure onInputChange and onButtonSubmit as props to ImageLinkForm
    * Add onInputChange method to onChange input property
    * Add onButtonSubmit method to onClick button property

--- src/App.js
+++ src/App.js
@@ -5,14 +5,31 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Count from './components/Count/Count';
 import './App.css';
 
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonSubmit = () => {
+    console.log('click')
+}
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Count />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p>{`This is the image link form`}</p>
       <div className='center'>
         <div className='form center'>
-          <input className='url-form' type='text' />
-          <button className='submit-button'>Hot Dog!!</button>
+          <input className='url-form' type='text' onChange={onInputChange}/>
+          <button className='submit-button' onClick={onButtonSubmit}>Hot Dog!!</button>
         </div>
       </div>
     </div>
