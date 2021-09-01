1e981169796cf833b85d935ba35f3f3e7ff5036c
cedis81 <cedisweb0@gmail.com>
2021-09-01 15:44:15 -0400

    Update SignIn component
    * Change component to implement state with email and password
    * Create onEmailChange method to record user's email
    * Create onPasswordChange method to record user's password
    * Create onSubmitSignIn to fetch post request to sign user in
    * onSubmitSignIn sends user's email and password
    * If successful, onSubmitSignIn loads user object and routes to 'home'
    * Add onEmailChange and onPasswordChange to form onClick
    * Add onSubmitSignIn to signup button

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,27 +1,66 @@
 import React from 'react';
 
-const SignIn = ({ onRouteChange }) => {
-  return (
-    <div className='auth-form'>
-      <h3>Sign In</h3>
-      <label htmlFor="email">Email</label>
-      <input
-        required
-        type="email"
-        name="email"
-        placeholder="Email"
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        required
-        name="password"
-        type="password"
-        placeholder="Password"
-      />
-      <button type="submit" onClick={() => onRouteChange('home')}>Sign In</button>
-      <button type="submit" onClick={() => onRouteChange('register')}>Register</button>
-    </div>
-  );
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      signInEmail: '',
+      signInPassword: ''
+    }
+  }
+
+  onEmailChange = (event) => {
+    this.setState({signInEmail: event.target.value})
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({signInPassword: event.target.value})
+  }
+
+  onSubmitSignIn = () => {
+    fetch('http://localhost:3000/signin', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      if (data.id) {
+        this.props.loadUser(data);
+        this.props.onRouteChange('home')
+      }
+    })
+  }
+
+  render() {
+    const { onRouteChange } = this.props
+    return (
+      <div className='auth-form'>
+        <h3>Sign In</h3>
+        <label htmlFor="email">Email</label>
+        <input
+          onChange={this.onEmailChange}
+          required
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          onChange={this.onPasswordChange}
+          required
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
+        <button type="submit" onClick={this.onSubmitSignIn}>Sign In</button>
+        <button type="submit" onClick={() => onRouteChange('register')}>Register</button>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
